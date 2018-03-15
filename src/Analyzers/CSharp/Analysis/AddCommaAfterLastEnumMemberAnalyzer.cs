96de31c45b591be702cce8a9c818e7fc54c6f150
Josef Pihrt <josef.pihrt@outlook.com>
2018-03-15 21:25:06 +0100

    x

--- src/Analyzers/CSharp/Analysis/AddCommaAfterLastEnumMemberAnalyzer.cs
+++ src/Analyzers/CSharp/Analysis/AddCommaAfterLastEnumMemberAnalyzer.cs
@@ -1,16 +1,33 @@
 ﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.Threading;
-using System.Threading.Tasks;
+using System;
+using System.Collections.Immutable;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Text;
 
-namespace Roslynator.CSharp.Refactorings
+namespace Roslynator.CSharp.Analysis
 {
-    internal static class AddCommaAfterLastEnumMemberAnalysis
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AddCommaAfterLastEnumMemberAnalyzer : BaseDiagnosticAnalyzer
     {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get { return ImmutableArray.Create(DiagnosticDescriptors.AddCommaAfterLastEnumMember); }
+        }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            base.Initialize(context);
+
+            context.RegisterSyntaxNodeAction(AnalyzeEnumDeclaration, SyntaxKind.EnumDeclaration);
+        }
+
         public static void AnalyzeEnumDeclaration(SyntaxNodeAnalysisContext context)
         {
             var enumDeclaration = (EnumDeclarationSyntax)context.Node;
