f0493e0f6a56b2ad3f7cc6ec842103b7df2c0923
Josef Pihrt <josef.pihrt@outlook.com>
2018-03-28 19:28:54 +0200

    x

--- src/CSharp/CSharp/CSharpFacts.cs
+++ src/CSharp/CSharp/CSharpFacts.cs
@@ -443,6 +443,7 @@ namespace Roslynator.CSharp
             }
         }
 
+        //TODO: IsEmbeddableStatement
         /// <summary>
         /// Returns true if a syntax of the specified kind can be an embedded statement.
         /// </summary>
